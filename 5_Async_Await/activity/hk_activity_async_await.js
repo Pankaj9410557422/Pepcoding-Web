0305dd575b710b38c8d7df90e63311fdb8399af3
Pankaj <engg.pankaj98@gmail.com>
2021-04-14 00:12:40 +0530

    converted hk code to async await

--- 5_Async_Await/activity/hk_activity_async_await.js
+++ 5_Async_Await/activity/hk_activity_async_await.js
@@ -2,64 +2,59 @@ let puppeteer = require("puppeteer");
 let {email, pass} = require("../../secret");
 let {codes} = require("./code")
 console.log("Before");
-(async function(){
-    try{
-    let browserInstance = await puppeteer.launch({
-        headless:false,
-        defaultViewport:null,
-        args:["--start-maximized"]
-    });
-    let newTab = await browserInstance.newPage();
-    await newTab.goto("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
-    await newTab.type("#input-1",email ,{ delay : 200});
-    await newTab.type("#input-2",pass,{ delay : 200});
-    await newTab.click("button[data-analytics='LoginPassword']");
-    await waitAndClick(".card-content h3[title='Interview Preparation Kit']",newTab);
-    await waitAndClick("a[data-attr1='warmup']",newTab);
-    let url = newTab.url();
-    for(let i=0;i<codes.length;i++){
-        await questionSolver(url,codes[i].soln, codes[i].qName,newTab);
+(async function () {
+    try {
+        let browserInstance = await puppeteer.launch({
+            headless: false,
+            defaultViewport: null,
+            args: ["--start-maximized",]
+        });
+        let newTab = await browserInstance.newPage();
+        await newTab.goto("https://www.hackerrank.com/auth/login?h_l=body_middle_left_button&h_r=login");
+        await newTab.type("#input-1", email, { delay: 200 });
+        await newTab.type("#input-2", pass, { delay: 200 });
+        await newTab.click("button[data-analytics='LoginPassword']");
+        await waitAndClick(".card-content h3[title='Interview Preparation Kit']", newTab);
+        await waitAndClick("a[data-attr1='warmup']", newTab);
+        let url = newTab.url();
+        for (let i = 0; i < codes.length; i++) {
+            await questionSolver(url, codes[i].soln, codes[i].qName, newTab);
+        }
+    } catch (err) {
+        console.log(err);
     }
-    console.log("Finished");
-}catch(error){
-    console.log(error);
-}
 })();
-
-async function waitAndClick(selector,newTab){
-        await newTab.waitForSelector(selector,{visible:true});
-        //we did not wait this promise coz we want the calling person to await this calling function
-        let selectorClickPromise = newTab.click(selector);
-        return selectorClickPromise;
+async function waitAndClick(selector, newTab) {
+    await newTab.waitForSelector(selector, { visible: true });
+    // we didn't wait this promise because we want  the calling perspn to await this promise based async function 
+    let selectorClickPromise = newTab.click(selector);
+    return selectorClickPromise;
 }
-async function questionSolver(url, code, questionname,gtab){
-    await gtab.goto(url);
-    //click as per question name
-    await gtab.evaluate(browserconsolefn,questionname);
-    //initially typing ans in custom input
-    await waitAndClick(".checkbox.input",gtab);
-    await gtab.type(".custominput",code);
+async function questionSolver(modulepageUrl, code, questionName, gtab) {
+    await gtab.goto(modulepageUrl);
+    function browserconsolerunFn(questionName) {
+        let allH4Elem = document.querySelectorAll("h4");
+        let textArr = [];
+        for (let i = 0; i < allH4Elem.length; i++) {
+            let myQuestion = allH4Elem[i]
+                .innerText.split("\n")[0];
+            textArr.push(myQuestion);
+        }
+        let idx = textArr.indexOf(questionName);
+        console.log(idx);
+        console.log("hello");
+        allH4Elem[idx].click();
+    }
+    await gtab.evaluate(browserconsolerunFn, questionName);
+    await waitAndClick(".custom-checkbox.inline", gtab);
+    await gtab.type(".custominput", code);
     await gtab.keyboard.down("Control");
     await gtab.keyboard.press("a");
     await gtab.keyboard.press("x");
-    await gtab.keyboard.up("Control");
-    //copy paste code ans in editor
-    await gtab.click(".hr-monaco-editor-parent");
-    await gtab.keyboard.down("Control");
+    await gtab.click(".monaco-editor.no-user-select.vs");
     await gtab.keyboard.press("a");
     await gtab.keyboard.press("v");
-    await gtab.keyboard.up("Control");
-    //click submit
     await gtab.click(".pull-right.btn.btn-primary.hr-monaco-submit");
-}
-function browserconsolefn(questionname){
-    let allh4Ele=document.querySelectorAll("h4"); 
-    let textarr=[];
-    for(let i=0;i<allh4Ele.length;i++){
-        let text=allh4Ele[i].innerText.split("\n")[0];
-        textarr.push(text);
-    }
-    
-    let idx = textarr.indexOf(questionname);
-    allh4Ele[idx].click();
+    return gtab.keyboard.up("Control");
+
 }
